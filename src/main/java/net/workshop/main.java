f2a04047a070c1b1c3e7fdb17187fe4711416f15
catanobro10 <78888788+catanobro10@users.noreply.github.com>
2022-01-17 13:40:18 -0800

    Initial commit

---
+++ src/main/java/net/workshop/main.java
@@ -0,0 +1,16 @@
+package net.workshop;
+import net.fabricmc.api.ModInitializer;
+
+public class main implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_NAME = "workshop";
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+	}
+}
